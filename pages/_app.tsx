9d880b1b13433c5e50fcba692d39c06d4e026e56
the-herald <owholdeman@gmail.com>
2025-10-18 12:53:57 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,7 @@
+import type { AppProps } from 'next/app';
+import '../styles/serp.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
 
